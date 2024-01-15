2fd346236e9c3313380634bac1f45efb63539808
Evelyn Asmat <evelyn.asmat@gmail.com>
2024-01-15 01:36:42 -0500

    Deploy

--- api/src/app.js
+++ api/src/app.js
@@ -15,7 +15,7 @@ server.use(bodyParser.json({ limit: '50mb' }));
 server.use(cookieParser());
 server.use(morgan('dev'));
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Origin', 'https://videogames-fma1.onrender.com');
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Expose-Headers', 'total-videogames');
--- api/src/db.js
+++ api/src/db.js
@@ -6,9 +6,15 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
+const DB_LOCAL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`;
+const sequelize = new Sequelize(DB_DEPLOY, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  dialectOptions: {
+    ssl: {
+      require: true,
+    },
+  },
 });
 const basename = path.basename(__filename);
 
