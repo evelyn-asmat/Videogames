7955f2a890feda3ccc871a23d49aa090d1e37705
Evelyn Asmat <evelyn.asmat@gmail.com>
2024-01-15 02:43:20 -0500

    local

--- api/src/db.js
+++ api/src/db.js
@@ -6,15 +6,9 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST, DB_DEPLOY
 } = process.env;
 
-const DB_LOCAL = `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`;
-const sequelize = new Sequelize(DB_DEPLOY, {
+const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/videogames`, {
   logging: false, // set to console.log to see the raw SQL queries
   native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-  dialectOptions: {
-    ssl: {
-      require: true,
-    },
-  },
 });
 const basename = path.basename(__filename);
 
